a21322918a9ba2fc3211843dbbc97ecc7bbd9848
Yaroslav Seriy <y.seriy@hostcomm.ru>
2016-05-10 10:13:42 +0300

    Add Sequence data type

--- lib/FMQueue/Data/Sequence.pm
+++ lib/FMQueue/Data/Sequence.pm
@@ -4,15 +4,36 @@ use strict;
 use warnings;
 
 sub new {
-    my ($class) = @_;
+    my ( $class, $coder ) = @_;
 
     my $self = {};
 
+    $self->{id}    = '';
     $self->{tasks} = [];
+    $self->{coder} = $coder;
 
     return bless $self, $class;
 }
 
+sub from_string {
+    my ( $self, $string ) = @_;
 
+    my $sequence = $coder->decode($string);
+
+    $self->{id}    = $sequence->{id};
+    $self->{tasks} = $sequence->{tasks};
+}
+
+sub id {
+    my ($self) = @_;
+
+    return $self->{id};
+}
+
+sub tasks {
+    my ($self) = @_;
+
+    return $self->{tasks};
+}
 
 1;
