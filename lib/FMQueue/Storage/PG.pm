2b1fea847c772adda8055d67aa1135ba13affc5c
Ярослав Серый <goodyar@yandex.ru>
2016-05-11 19:16:57 +0300

    Переработка хранилища для работы с последовательностями задач

--- lib/FMQueue/Storage/PG.pm
+++ lib/FMQueue/Storage/PG.pm
@@ -4,10 +4,9 @@ use strict;
 use warnings;
 
 use DBI;
-use Data::UUID;
 
 sub new {
-    my ( $class, $config ) = @_;
+    my ($class) = @_;
 
     my $self = {};
 
@@ -15,29 +14,36 @@ sub new {
     $self->{user} = '';
     $self->{pass} = '';
     $self->{dbh}  = '';
-    $self->{config} = $config;
-    $self->{uuid}   = Data::UUID->new;
 
     return bless $self, $class;
 }
 
-sub read_config {
-    my ($self) = @_;
+sub dsn {
+    my ( $self, $dsn ) = @_;
 
-    my $parameters = $self->{config}->parameters;
+    $self->{dsn} = $dsn if $dsn;
 
-    $self->{dsn} = "dbi:Pg:dbname=$parameters->{dbname};"
-                 . "host=$parameters->{host};"
-                 . "port=$parameters->{port};";
+    return $self;
+}
 
-    $self->{user} = $parameters->{user} || '';
-    $self->{pass} = $parameters->{pass} || '';    
+sub user {
+    my ( $self, $user ) = @_;
+
+    $self->{user} = $user if $user;
+
+    return $self;
+}
+
+sub pass {
+    my ( $self, $pass ) = @_;
+
+    $self->{pass} = $pass if $pass;
+
+    return $self;
 }
 
 sub connect {
     my ($self) = @_;
-
-    $self->read_config;
 
     $self->{dbh} = DBI->connect(
         $self->{dsn},
@@ -63,56 +69,96 @@ sub reconnect {
     $self->connect if ! $self->{dbh}->ping;
 }
 
-sub start_transaction {
-    my ( $self, $body, $account ) = @_;
+sub insert_sequence {
+    my ( $self, $sequence ) = @_;
 
     my $table1 = q{INSERT INTO transactions
                     (id,status,start_timestamp,status_timestamp,account)
                         VALUES (?,?,current_timestamp,current_timestamp,?)};
     my $table2 = q{INSERT INTO tasks
-                    (id,transaction_id,status, 
-                        command,start_timestamp,status_timestamp)
-                            VALUES (?,?,?,?,current_timestamp,current_timestamp)};
+                    (id,transaction_id,status,command,step,start_timestamp,
+                        status_timestamp)
+                            VALUES(?,?,?,?,?,current_timestamp,
+                                current_timestamp)};
 
-    my $uuid1 = $self->{uuid}->create_str;
-    my $uuid2 = $self->{uuid}->create_str;
-
-    $self->{dbh}->begin_work;
-
-    $self->{dbh}->do( $table1, undef, ( $uuid1, 'runnig', $account ) );
-    $self->{dbh}->do( $table2, undef, ( $uuid2, $uuid1, 'ready', $body ) );
-    
-    $self->{dbh}->commit;
-
-    return $uuid1;
-}
-
-sub update_transaction {
-    my ( $self, $id, $task_id, $result ) = @_;
-
-    my $table1 = q{UPDATE transactions SET status = ?,
-                    status_timestamp = current_timestamp WHERE id = ?};
-    my $table2 = q{UPDATE tasks SET status = ?, status_text = ?,
-                    status_timestamp = current_timestamp WHERE id = ?};
-
-    my $task_status = $result->{rc} ? 'error' : 'comlete';
-    my $transaction_status = $task_status;
+    my $sequence_id = $sequence->id;
+    my $user_id     = $sequence->user_id;
+    my $tasks       = $sequence->tasks;
 
     $self->{dbh}->begin_work;
 
-    $self->{dbh}->do( $table1, undef, ( $transaction_status, $id ) );
-    $self->{dbh}->do( $table2, undef, ( $task_status, $result->{text}, $task_id ) );
-    
+    $self->{dbh}->do( $table1, undef, ( $sequence_id, 'runnig', $user_id ) );
+
+    my $step = 0;
+    foreach my $task (@{$tasks}){
+        $step += 1;
+
+        my $status = ($step == 1) ? 'ready' : 'new';
+
+        $self->{dbh}->do(
+            $table2,
+            undef,
+            ( $task->task_id, $task->seq_id, $status, $task->to_string, $step )
+        );
+    }
+
     $self->{dbh}->commit;
 }
 
-sub update_task {
-    my ( $self, $task_id, $status ) = @_;
+sub update_sequence {
+    my ( $self, $task ) = @_;
+
+    $self->{dbh}->begin_work;
+
+    if ( ! $task->result->{rc} ) {
+        $self->set_sequence_status( $task->seq_id, 'running');
+        $self->set_task_status( $task->task_id, 'complete', $task->result->{text} );
+        $self->get_next_step($task->seq_id);
+    }
+    else {
+
+    }
+
+    $self->{dbh}->commit;
+}
+
+sub set_sequence_status {
+    my ( $self, $seq_id, $status ) = @_;
+
+    my $table = q{UPDATE transactions SET status = ?,
+                    status_timestamp = current_timestamp WHERE id = ?};
+
+    $self->{dbh}->do( $table, undef, ( $status, $seq_id ) );
+}
+
+sub set_task_status {
+    my ( $self, $task_id, $status, $status_text ) = @_;
+
+    my $tasks_table = q{UPDATE tasks SET status = ?, status_text = ?,
+                    status_timestamp = current_timestamp WHERE id = ?};
+
+    $self->{dbh}->do( $tasks_table, undef, ( $status, $task_id, $status_text ) );
+}
+
+sub get_next_step {
+    my ( $self, $seq_id ) = @_;
+
+    my $select = q{SELECT id FROM tasks WHERE transaction_id = ? AND step IN
+                    (SELECT MIN(step) FROM tasks
+                        WHERE transaction_id = ? AND status = 'new')};
+
+    my $task = $self->{dbh}->selectrow_hashref( $select, undef, ($seq_id) );
+
+    return $task->{id} ? $self->set_next_step($task->{id}) : 0;
+}
+
+sub set_next_step {
+    my ( $self, $task_id ) = @_;
 
     my $tasks_table = q{UPDATE tasks SET status = ?,
                     status_timestamp = current_timestamp WHERE id = ?};
 
-    $self->{dbh}->do( $tasks_table, undef, ( $status, $task_id ) );    
+    return $self->{dbh}->do( $tasks_table, undef, ( 'ready', $task_id ) );
 }
 
 sub get_ready_tasks {
